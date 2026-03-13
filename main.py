86837b16b5e96ad55880431deb2f9fc52864f5ef
Abid Mustaghfirin <133614198+abid-cyber@users.noreply.github.com>
2026-03-13 23:29:59 +0700

    Update: ganti nama dan bersihkan komentar

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import redis
 import time
 
-# Koneksi ke Redis
 r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
-# Cek koneksi
 print("Ping:", r.ping())
 
-# 1. SET - Menyimpan data
-r.set("user:1", "John Doe")
+r.set("user:1", "Abid Mustaghfirin")
 print("SET user:1 =", r.get("user:1"))
 
-# 2. GET - Mengambil data
 nama = r.get("user:1")
 print("GET user:1 =", nama)
 
-# 3. DEL - Menghapus data
 r.delete("user:1")
 print("DEL user:1 -> GET user:1 =", r.get("user:1"))
 
-# 4. EXPIRE - Set waktu kadaluarsa
 r.set("session:1", "active")
-r.expire("session:1", 5)  # kadaluarsa dalam 5 detik
+r.expire("session:1", 5)  
 print("SET session:1 = active (expire 5 detik)")
 print("TTL session:1 =", r.ttl("session:1"), "detik")
 
